0d2dc14b2bb53c34f8373cffd3ecb758ce8fe3de
Raylee <abramscarter1999@gmail.com>
2026-07-15 17:09:09 -0400

    add more to config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ window_size = (700, 450)
 
 # Text settings
 font = "Noto Sans Mono" # Currently other fonts have not been tested and may cause rendering issues
-text_color = [255,255,255]
+text_color = (255,255,255)
 font_size = 17
 line_height = 20
 
@@ -16,15 +16,20 @@ padding_top = 1
 padding_left = 3
 
 # Context menu (default at bottom w/ position locked for now)
-context_info_color = [111, 110, 115]
+context_info_color = (111, 110, 115)
 context_info_size = 15
 context_info_padding_bottom = 2
-context_background_color = [22, 22, 28]
+context_background_color = (22, 22, 28)
 context_background_padding_bottom = 2
 
-unsaved_alert_color = [68, 73, 122]
+unsaved_alert_color = (68, 73, 122)
 unsaved_alert_size = 5
 unsaved_alert_corner_padding = 11
 
 # Cursor settings
-cursor_color = [255, 255, 255]
+cursor_color = (255, 255, 255)
+large_step = 5
+
+# Arrow key repeating delay (ms)
+initial_delay = 430
+repeat_time = 31
