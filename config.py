98f420b2aee06b9d8cdb85885b7c1e2ac194359b
Raylee <abramscarter1999@gmail.com>
2026-07-15 12:54:49 -0400

    add more config options in config.py

--- config.py
+++ config.py
@@ -1,20 +1,30 @@
-# Default Reed configuration
+### Default Reed configuration ###
 # If any variables from this file are deleted, the program will not run
 
+# General Window Settings
 background_color = [39,40,43]
 window_size = (700, 450)
 
-font = "Noto Sans Mono"
+# Text settings
+font = "Noto Sans Mono" # Currently other fonts have not been tested and may cause rendering issues
 text_color = [255,255,255]
 font_size = 17
 line_height = 20
 
-# Context menu (default at bottom)
+# Padding on text position
+padding_top = 1
+padding_left = 3
+
+# Context menu (default at bottom w/ position locked for now)
 context_info_color = [111, 110, 115]
 context_info_size = 15
-context_background_color = [22, 22, 28]
 context_info_padding_bottom = 2
+context_background_color = [22, 22, 28]
 context_background_padding_bottom = 2
 
-# cursor settings
+unsaved_alert_color = [68, 73, 122]
+unsaved_alert_size = 5
+unsaved_alert_corner_padding = 11
+
+# Cursor settings
 cursor_color = [255, 255, 255]
