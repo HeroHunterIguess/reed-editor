550484125a3775d63b3890f3e8c567de76b674a6
Raylee <abramscarter1999@gmail.com>
2026-07-15 17:28:06 -0400

    remove rendering from main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 ### Reed, a minimalistic, customizable text editor ###
 
 # Initial set up 
-import sys, os, pygame, pygame.locals, utils, config as c
+import sys, os, pygame, pygame.locals, utils, rendering, config as c
 pygame.init()
 pygame.font.init()
 clock = pygame.time.Clock()
+
+# Intial window steup
+screen = pygame.display.set_mode(c.window_size)
+pygame.display.set_caption("Reed editor")
 
 cursor_location = [0, 0]
 last_y = 0
@@ -23,15 +27,6 @@ if len(sys.argv) > 1:
     filepath = sys.argv[1]
 
 ##########################
-
-# Intial window steup
-screen = pygame.display.set_mode(c.window_size)
-pygame.display.set_caption("Reed editor")
-
-font = pygame.font.SysFont(c.font, c.font_size)
-context_font = pygame.font.SysFont(c.font, c.context_info_size)
-
-char_width = font.size(" ")[0]
 
 # Create new buffer for everything in the file
 buffer = [list(line) for line in utils.read_file(filepath).splitlines()]
@@ -52,39 +47,8 @@ while running:
     # Limit fps
     dt = clock.tick(60)
 
-    # Set background
-    screen.fill(c.background_color)
-
-    # Start text at the top of the screen (+ padding)
-    y = c.padding_top
-
-    # Render main text
-    for line_chars in buffer:
-
-        # Join into full lines
-        line = "".join(line_chars)
-
-        # Display it
-        text_surface = font.render(line, True, c.text_color)
-        screen.blit(text_surface, (c.padding_left,y))
-
-        y += c.line_height
-
-    ##########################
-
-    # Display cursor @ location * char_width, with a set width of 2, and height being the line height 
-    pygame.draw.rect(screen, c.cursor_color, (cursor_location[1] * char_width - 1 + c.padding_left, cursor_location[0] * c.line_height + 4, 2, c.font_size))
-
-    # Display context menu at the bottom
-    pygame.draw.rect(screen, c.context_background_color, (0, c.window_size[1] - c.line_height - c.context_background_padding_bottom, c.window_size[0], c.line_height + c.context_background_padding_bottom))
-
-    # Draw the text for the menu
-    context_text = context_font.render(filepath, True, c.context_info_color)
-    screen.blit(context_text, (0, c.window_size[1] - c.line_height - c.context_info_padding_bottom))
-
-    # Draw the alert on the context menu if the file has unsaved changes
-    if changed:
-        pygame.draw.circle(screen, c.unsaved_alert_color, (c.window_size[0] - c.unsaved_alert_corner_padding, c.window_size[1] - c.unsaved_alert_corner_padding), c.unsaved_alert_size)
+    # Draw screen
+    rendering.draw(screen, buffer, cursor_location, filepath, changed)
 
     ##########################
 
