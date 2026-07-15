11c5bde79dd118e00141f84c01768344b58e1fc0
Raylee <abramscarter1999@gmail.com>
2026-07-15 11:11:05 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# Default Reed configuration
+
+background_color = [39,40,43]
+window_size = (700, 450)
+
+font = "Noto Sans Mono"
+text_color = [255,255,255]
+font_size = 17
+line_height = 20
+
+# Context menu (default at bottom)
+context_info_color = [111, 110, 115]
+context_info_size = 15
+context_background_color = [22, 22, 28]
+context_info_padding_bottom = 2
+context_background_padding_bottom = 2
+
+# cursor settings
+cursor_color = [255, 255, 255]
