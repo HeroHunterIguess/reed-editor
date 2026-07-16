4f5d9b795c9a20206b682bccb00838c4f574553b
Raylee <abramscarter1999@gmail.com>
2026-07-16 18:38:33 -0400

    add more config options and improved defaults in config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,12 +1,20 @@
 ### Default Reed configuration ###
 # If any variables from this file are deleted, the program will not run
 
+import os
+
 # General Window Settings
 background_color = [39,40,43]
-window_size = (700, 450)
+window_size = (1000, 650)
 
 # Text settings
-font = "Noto Sans Mono" # Currently other fonts have not been tested and may cause rendering issues
+# Only monospace fonts will work properly
+# Default based on operating system defaults
+if os.name == "posix":
+    font = "Monospace" 
+elif os.name == "nt":
+    font = "Cascadia Code"
+
 text_color = (255,255,255)
 font_size = 17
 line_height = 20
@@ -15,14 +23,14 @@ line_height = 20
 padding_top = 1
 padding_left = 3
 
-# Context menu (default at bottom w/ position locked for now)
+# Context menu (default bottom w/ position locked for now)
 context_info_color = (111, 110, 115)
 context_info_size = 15
 context_info_padding_bottom = 2
 context_background_color = (22, 22, 28)
 context_background_padding_bottom = 2
 
-unsaved_alert_color = (68, 73, 122)
+unsaved_alert_color = (98, 103, 152)
 unsaved_alert_size = 5
 unsaved_alert_corner_padding = 11
 
@@ -30,6 +38,10 @@ unsaved_alert_corner_padding = 11
 cursor_color = (255, 255, 255)
 large_step = 5
 
-# Arrow key repeating delay (ms)
+# Input repeating delay (ms)
 initial_delay = 430
 repeat_time = 31
+
+# View movement (pixels)
+view_move_amount = 20
+view_padding = 100
