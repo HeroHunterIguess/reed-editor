3b61b346c170819cf10a990704d02d389cbb0b2b
Raylee <abramscarter1999@gmail.com>
2026-07-18 13:58:59 -0400

    add new v0.0.3 versions

---
+++ v0.0.3-python/config_manager.py
@@ -0,0 +1,54 @@
+### Initializing configuration ###
+
+
+from pathlib import Path
+import os, shutil, importlib.util
+
+# TESTING OPTION TO ALWAYS SOURCE FROM DEFAULT
+use_default_config = True
+
+def get_config_path():
+    if os.name == "nt": # windows
+        config_directory = Path(os.environ["APPDATA"]) / "reed" / "config.py"
+    elif os.name == "posix": # unix-based
+        config_directory = Path.home() / ".config" / "reed" / "config.py"
+    
+    # Override config path and use default
+    if use_default_config:
+        return "default_config.py"
+    
+    return config_directory
+
+def initialize_config():
+    # get path
+    config_path = get_config_path()
+
+    # If using real config then create it if the dir doesnt exist
+    if not use_default_config:
+        config_path.parent.mkdir(parents=True, exist_ok=True)
+
+    # fill config file
+    try:
+        with open(config_path, "x") as main_config, open ("default_config.py", "r") as default_config:
+            for line in default_config:
+                main_config.write(line)
+        
+        # load config after file is created
+        load_config(config_path)
+    
+    except FileExistsError:
+        load_config(config_path)
+
+def load_config(config_path):
+    # load config into local easy to access file
+    with open(config_path, "r") as remote_config:
+        try:
+            with open("config.py", "w") as local_config:
+                for line in remote_config:
+                    local_config.write(line)
+        
+        # create local config if it doesnt exist
+        except FileNotFoundError:
+            with open("config.py", "x") as local_config:
+                for line in remote_config:
+                    local_config.write(line)
---
+++ v0.0.3-python/editing.py
@@ -0,0 +1,139 @@
+### Editng functions ###
+
+
+import config as c
+
+# Handle hitting enter for a new line
+def new_line(s):
+    # Try-except incase the line is empty
+    try:
+        len(s.buffer[s.cursor_location[0]])
+
+        # Split line into parts to seperate them
+        current_line = s.buffer[s.cursor_location[0]]
+        left = current_line[ : s.cursor_location[1]]
+        right = current_line[s.cursor_location[1]:]
+
+        # Split the two onto different lines
+        s.buffer[s.cursor_location[0]] = left
+        s.buffer.insert(s.cursor_location[0] + 1, right)
+    except IndexError:
+        # If the line is empty then create new empty line
+        s.buffer.append([])
+
+    # Fix cursor location
+    s.cursor_location[0] += 1
+    s.cursor_location[1] = 0
+
+# Handle backspacing cases
+def backspace(s, holding_ctrl):
+    line_num = s.cursor_location[0]
+    line = s.buffer[line_num]
+
+    # On first character (& not first line)
+    # Runs even if you hit control backspace
+    if s.cursor_location[1] == 0 and line_num != 0: 
+
+        previous_line_length = len(s.buffer[line_num - 1])
+        
+        # Move characters to previous line
+        s.buffer[line_num - 1].extend(line)
+
+        # Remove current line
+        s.buffer.pop(line_num)
+        line_num -= 1
+
+        # Fix cursor placement
+        s.cursor_location[0] -= 1
+        s.cursor_location[1] = previous_line_length
+    
+    # In a normal case delete 1 character
+    elif s.cursor_location[1] != 0:
+        del s.buffer[line_num][s.cursor_location[1] - 1]
+        s.cursor_location[1] -= 1
+    
+    ##########################
+
+    # Runs control backspace even after deleting first char already - This is intended
+    if holding_ctrl:
+        count = 0
+        deleted_spaces = False
+
+        # Delete spaces
+        if s.cursor_location[1] > 0 and s.buffer[line_num][s.cursor_location[1] - 1] == " ":
+            for char in reversed(line[ : s.cursor_location[1]]):
+                if char == " ":
+                    count += 1
+                else:
+                    deleted_spaces = True
+                    break
+
+        # Iterate through looking for word
+        if not deleted_spaces:
+            for char in reversed(line[ : s.cursor_location[1]]):
+                if char != " ":
+                    count += 1
+                else:
+                    break
+
+        # Delete word and update buffer & cursor pos
+        del s.buffer[line_num][s.cursor_location[1] - count : s.cursor_location[1]]
+        s.cursor_location[1] -= count
+
+# Handle hitting delete key 
+def delete(s, holding_ctrl):
+    line_num = s.cursor_location[0]
+    line = s.buffer[line_num]
+
+    # On last character
+    # Runs even if you hit control delete
+    if s.cursor_location[1] == len(line) and line_num < len(s.buffer) - 1: 
+        
+        # Move characters from previous line
+        s.buffer[line_num].extend(s.buffer[line_num + 1])
+
+        # Remove next line
+        s.buffer.pop(line_num + 1)
+    
+    # In a normal case delete 1 character
+    elif s.cursor_location[1] < len(s.buffer[s.cursor_location[0]]):
+        del s.buffer[line_num][s.cursor_location[1]]
+    
+    ##########################
+
+    # Runs control delete even after deleting first char already - This is intended
+    if holding_ctrl:
+        count = 0
+        deleted_spaces = False
+
+        # Delete spaces
+        if s.cursor_location[1] < len(line) and s.buffer[line_num][s.cursor_location[1]] == " ":
+            for char in line[s.cursor_location[1] : ]:
+                if char == " ":
+                    count += 1
+                else:
+                    deleted_spaces = True
+                    break
+
+        # Iterate through looking for word
+        if not deleted_spaces:
+            for char in line[s.cursor_location[1] : ]:
+                if char != " ":
+                    count += 1
+                else:
+                    break
+
+        # Delete word and update buffer & cursor pos
+        del s.buffer[line_num][s.cursor_location[1] : s.cursor_location[1] + count]
+
+# Insert given unicode character
+def insert_character(s, event):
+    s.buffer[s.cursor_location[0]].insert(s.cursor_location[1], event.unicode)
+    s.cursor_location[1] += 1
+
+# Insert tab spaces
+def tab(s):
+    for i in range(c.tab_spaces):   
+
+        s.buffer[s.cursor_location[0]].insert(s.cursor_location[1], " ")
+        s.cursor_location[1] += 1
